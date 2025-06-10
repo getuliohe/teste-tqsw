f560be9e9f1c70eccb4dd1a601599cb480d5ef1f
Getulio Lima <getuliofurtado03@gmail.com>
2025-06-10 08:29:11 -0300

    FIX: fixing main.yml

--- tests/e2e/course.test.js
+++ tests/e2e/course.test.js
@@ -1,10 +1,9 @@
 const request = require('supertest');
-const app = require('../../app'); // Exporte seu app Express em app.js
+const app = require('../../app');
 const sequelize = require('../../config/database');
 
 describe('Fluxo de Cursos', () => {
   beforeAll(async () => {
-    // Limpar e popular o banco de dados de teste antes dos testes
     await sequelize.sync({ force: true });
   });
 
@@ -19,7 +18,6 @@ describe('Fluxo de Cursos', () => {
     expect(response.statusCode).toBe(302);
     expect(response.headers.location).toBe('/courses/list');
 
-    // Opcional: Verificar se o curso foi realmente inserido no banco de dados
     const course = await sequelize.models.Course.findOne({ where: { name: 'Novo Curso de Teste' } });
     expect(course).not.toBeNull();
     expect(course.description).toBe('Descrição do novo curso');
