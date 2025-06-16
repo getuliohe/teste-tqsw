f77b209689c732c1fd35c1b012e172354e429429
Daniel <82069353+dneil-143@users.noreply.github.com>
2025-06-15 22:28:28 -0300

    Update Student.js

--- models/Student.js
+++ models/Student.js
@@ -2,7 +2,7 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
-const Student = sequelize.define('Student', {
+const Student = sequelize.define('Student', {//
     name: {
         type: DataTypes.STRING,
         allowNull: false,
