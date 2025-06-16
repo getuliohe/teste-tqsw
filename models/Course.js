75310c749f600b980454618124aa1c4aae48d369
Daniel <82069353+dneil-143@users.noreply.github.com>
2025-06-15 22:33:40 -0300

    Update Course.js

--- models/Course.js
+++ models/Course.js
@@ -1,20 +1,30 @@
 // models/Course.js
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
+'use strict';
+const { Model } = require('sequelize');
 
-const Course = sequelize.define('Course', {
+module.exports = (sequelize, DataTypes) => {
+  class Course extends Model {
+    // Define a associação: um curso pode ter muitos estudantes
+    static associate(models) {
+      Course.hasMany(models.Student, {
+        foreignKey: 'courseId',
+        as: 'students'
+      });
+    }
+  }
+  Course.init({
     name: {
-        type: DataTypes.STRING,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false
     },
     description: {
-        type: DataTypes.STRING,
-        allowNull: true,
-    },
-}, {
-    // Adicione esta seção para especificar o nome da tabela
-    tableName: 'courses',
-    timestamps: true
-});
-
-module.exports = Course;
+      type: DataTypes.STRING,
+      allowNull: true
+    }
+  }, {
+    sequelize,
+    modelName: 'Course',
+    tableName: 'courses' // Manter a especificação do nome da tabela
+  });
+  return Course;
+};
