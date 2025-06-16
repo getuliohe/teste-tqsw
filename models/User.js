46894ae13ca527056048a82d1779df8fc4225fff
Daniel <82069353+dneil-143@users.noreply.github.com>
2025-06-15 22:19:18 -0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,31 +1,25 @@
-'use strict';
+// models/User.js
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
 
-const User = (sequelize, DataTypes) => {
-  const model = sequelize.define('User', {
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-    },
+const User = sequelize.define('User', {
     name: {
         type: DataTypes.STRING,
-        allowNull: false,
+        allowNull: false
     },
     email: {
         type: DataTypes.STRING,
         allowNull: false,
-        unique: true,
+        unique: true
     },
     password: {
         type: DataTypes.STRING,
-        allowNull: false,
-    },
-  }, {
-    tableName: 'User',
-    timestamps: true,
-  });
-
-  return model;
-};
+        allowNull: false
+    }
+}, {
+    // Adicione esta seção para especificar o nome da tabela
+    tableName: 'users',
+    timestamps: true // Recomendo manter, pois suas migrations criam os campos
+});
 
 module.exports = User;
