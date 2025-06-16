a9296f9ab7681d82a61b80a65581a672eeafdba0
Daniel <82069353+dneil-143@users.noreply.github.com>
2025-06-15 21:43:21 -0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -17,8 +17,8 @@ module.exports = {
       dialect: "mysql",
       logging: false,
       // Adicione estas duas linhas:
-      charset: 'utf8',
-      collate: 'utf8_general_ci'
+      charset: 'utf8mb4',
+      collate: 'utf8mb4_general_ci'
     },
   production: {
     use_env_variable: "DATABASE_URL",
